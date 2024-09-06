231b868f04d5bdd12d1a52dc3f8c28e86e47319f
KachmaZ <boss.kachmaz@mail.ru>
2024-09-06 16:06:26 +0300

    Install Vuetify and Yup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+
+// I18n
 import { createI18n } from 'vue-i18n'
 import { languages } from './i18n'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const i18n = createI18n({
     legacy: false,
@@ -13,4 +21,9 @@ const i18n = createI18n({
     }
 })
 
-createApp(App).use(i18n).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+createApp(App).use(i18n).use(vuetify).mount('#app')
