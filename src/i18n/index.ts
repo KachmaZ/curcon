01eb29059679c8381a719cc58be003713e598d8f
KachmaZ <boss.kachmaz@mail.ru>
2024-09-06 19:52:18 +0300

    Install prettier. Additional scripts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,9 @@
-import en from './en.json'
-import ru from './ru.json'
+import en from './en.json';
+import ru from './ru.json';
 
 type MessageSchema = typeof en;
 
 export const messages: Record<string, MessageSchema> = {
     en,
-    ru
-}
+    ru,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from 'vue';
+import './style.css';
+import App from './App.vue';
 
 // I18n
-import { createI18n } from 'vue-i18n'
-import { messages } from './i18n'
+import {createI18n} from 'vue-i18n';
+import {messages} from './i18n';
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import 'vuetify/styles';
+import {createVuetify} from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const i18n = createI18n({
     legacy: false,
     locale: 'ru',
     fallbackLocale: 'en',
     messages: {
-        ...messages
-    }
-})
+        ...messages,
+    },
+});
 
 const vuetify = createVuetify({
     components,
     directives,
-  })
+});
 
-createApp(App).use(i18n).use(vuetify).mount('#app')
+createApp(App).use(i18n).use(vuetify).mount('#app');
